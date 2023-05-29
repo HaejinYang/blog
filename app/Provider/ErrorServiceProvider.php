65e9e3322ec960f327186e40b4abae85aaf000d2
haejin <haejin.yang91@gmail.com>
2023-05-29 11:23:32 +0900

    feat: 서버 디비 연결 확인

--- app/Provider/ErrorServiceProvider.php
+++ app/Provider/ErrorServiceProvider.php
@@ -11,7 +11,7 @@ class ErrorServiceProvider extends ServiceProvider
         });
 
         set_exception_handler(function ($e){
-            error_log($e.PHP_EOL, 3, dirname(__DIR__, 2).'/storage/logs/logs.log');
+            error_log("[".date('y-m-d H:i:s')."]".$e.PHP_EOL, 3, dirname(__DIR__, 2).'/storage/logs/logs.log');
         });
     }
 
