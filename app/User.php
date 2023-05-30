9487bcbadc854f9666896f9b0ebdc7ae356373c7
haejin <haejin.yang91@gmail.com>
2023-05-30 15:33:06 +0900

    feat:  유저 등록 추가

--- app/User.php
+++ app/User.php
@@ -2,10 +2,17 @@
 
 namespace App;
 
+use Thumbsupcat\IcedAmericano\Database\Adaptor;
+
 class User
 {
-    public function getUsername()
+    public function create(): bool
     {
-        var_dump($this->email);
+        return Adaptor::exec("INSERT INTO user(`email`, `password`) VALUES(?, ?)", [$this->email, $this->password]);
+    }
+
+    public function getUsername(): string
+    {
+        return current(explode('@', $this->email));
     }
 }
