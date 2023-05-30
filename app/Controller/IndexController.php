644586e9849d1f44980620b4d969ec8d783aa98d
haejin <haejin.yang91@gmail.com>
2023-05-30 15:40:58 +0900

    feat: IndexController에서 page가 null일 때 버그 수정

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -10,7 +10,8 @@ class IndexController
 {
     public static function index()
     {
-        $page = filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT);
+        $page = filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT) ?? 0;
+
         return Theme::view('index', [
             'posts' => IndexService::getPosts($page, 3)
         ]);
